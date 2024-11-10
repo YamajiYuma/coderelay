1e6babe2503ff43501b90c908567ec2c64f8d42b
Yuma Yamaji <121208581+YamajiYuma@users.noreply.github.com>
2024-11-10 15:49:49 +0900

    Update edit.js

--- Js/edit.js
+++ Js/edit.js
@@ -24,6 +24,15 @@ const database = getDatabase(app);
 
 setupPlayer2();
 
+const explain1Element = document.querySelector(".explain1");
+// explanation1を表示
+onValue(ref(database, "game/explanation1"), (snapshot) => {
+  const data = snapshot.val();
+  if (data && data.explanation1) {
+    explain1Element.textContent = `説明1: ${data.explanation1}`;
+  }
+});
+
 // プレイヤー2の設定
 function setupPlayer2() {
   const runButton = document.getElementById("run-btn");
